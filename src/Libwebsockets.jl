f97a8c71c7f1f793f19abacdc03353159eef26ba
kshilin <kshilin@bhft.com>
2025-10-30 12:34:04 +0100

    external+internal consts

--- src/Libwebsockets.jl
+++ src/Libwebsockets.jl
@@ -3,9 +3,7 @@ module Libwebsockets
 using libwebsockets_jll
 using Printf
 
-include("http_consts.jl")
-include("lws_consts.jl")
-include("wsi_consts.jl")
+include("external_consts.jl")
 include("internal_consts.jl")
 include("types.jl")
 include("functions.jl")
